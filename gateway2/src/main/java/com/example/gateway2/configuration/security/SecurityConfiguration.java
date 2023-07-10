142898b5a7cfddb4b4cfa8da41599b4688f1cbfb
hieuaxz00001 <45386604+hieuaxz00001@users.noreply.github.com>
2023-07-11 00:49:27 +0700

    hehe

--- gateway2/src/main/java/com/example/gateway2/configuration/security/SecurityConfiguration.java
+++ gateway2/src/main/java/com/example/gateway2/configuration/security/SecurityConfiguration.java
@@ -1,21 +1,94 @@
 package com.example.gateway2.configuration.security;
 
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cglib.core.Converter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfiguration {
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .httpBasic()
-                .and()
-                .csrf()
-                .disable()
-                .build();
+    SecurityFilterChain filterChain(HttpSecurity http, @Qualifier("") Converter jwtAuthenticationConverter) throws Exception {
+
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+        // Enable and configure CORS
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource("http://localhost:4200")));
+
+        // State-less session (state in access-token only)
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Disable CSRF because of state-less session-management
+        http.csrf(csrf -> csrf.disable());
+
+        // Return 401 (unauthorized) instead of 302 (redirect to login) when
+        // authorization is missing or invalid
+        http.exceptionHandling(eh -> eh.authenticationEntryPoint((request, response, authException) -> {
+            response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Restricted Content\"");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        }));
+
+        // @formatter:off
+        http.authorizeHttpRequests(accessManagement -> accessManagement
+                .requestMatchers("/actuator/health/readiness", "/actuator/health/liveness", "/v3/api-docs/**","/public").permitAll()
+                .anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(oauth2Configurer -> oauth2Configurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = realmAccess.get("roles");
+            var grantedAuthorities = roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+            return new JwtAuthenticationToken(jwt, grantedAuthorities);
+        })));
+        // @formatter:on
+
+        return http.build();
     }
+
+    private UrlBasedCorsConfigurationSource corsConfigurationSource(String... origins) {
+        final var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(origins));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 }
